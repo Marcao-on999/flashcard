c04df601c01b6f801b9caf20bdd308d61b2f7dec
Marcao-on999 <rochamarcon@gmail.com>
2024-11-13 11:37:01 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Transformers',
+    'Quem é o líder dos Autobots no universo de Transformers?',
+    'O líder dos Autobots é Optimus Prime, um dos personagens mais icônicos da franquia'
+)
+
+criaCartao(
+    'Transformers',
+    'Quem é o principal antagonista dos Autobots em Transformers?',
+    'O principal antagonista dos Autobots é Megatron, líder dos Decepticons'
+)
+
+criaCartao(
+    'Transformers',
+    'Qual é a habilidade principal dos Transformers?',
+    'A habilidade principal dos Transformers é a capacidade de se transformar em diferentes tipos de veículos, máquinas ou animais, além de sua força e habilidades de combate'
+)
+
+criaCartao(
+    'Transformers',
+    'Qual ator interpreta o personagem Sam Witwicky em Transformers?',
+    'Sam Witwicky é interpretado por Shia LaBeouf nos primeiros filmes da franquia'
+)
